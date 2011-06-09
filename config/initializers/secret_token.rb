90a8a4b5e0a738f150cc488b1537558d0962347a
Dan Reedy <danreedy@gmail.com>
2011-06-09 08:16:35 -0500

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AchImport::Application.config.secret_token = '0e382116b89570087e34eb313416babf08dbea5288cfbb32b87bec37613b05630d94629eb344216d8e265ba8b053cb331a5a3cc28b8792346e615c894ae433cc'
